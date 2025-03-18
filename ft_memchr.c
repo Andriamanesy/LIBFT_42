1b575117edc527e5a8c32322322650bd74dff374
Brian Andriamanesy <briandri@c1r5s14.42antananarivo.mg>
2025-03-18 15:51:20 +0300

    Correction split

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: briandri <briandri@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 08:55:54 by briandri          #+#    #+#             */
-/*   Updated: 2025/03/18 14:51:27 by briandri         ###   ########.fr       */
+/*   Updated: 2025/03/18 15:49:42 by briandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*cast;
-	unsigned int	i;
+	size_t			i;
 
 	i = 0;
 	cast = (unsigned char *)s;
 	while (i < n)
 	{
-		if ((c >= 256 && (cast[i] == (char)c)) || (cast[i] == (char)c))
-			return (&cast[i]);
+		if (cast[i] == (char)c)
+			return ((void *)(cast + i));
 		i++;
 	}
 	return (0);
--- ft_split.c
+++ ft_split.c
@@ -6,84 +6,57 @@
 /*   By: briandri <briandri@student.42antanana      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 14:16:18 by briandri          #+#    #+#             */
-/*   Updated: 2025/03/15 14:16:22 by briandri         ###   ########.fr       */
+/*   Updated: 2025/03/18 15:40:40 by briandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 static int	ft_count_words(char const *str, char c);
-static int	ft_strlen_sep(char const *s, char c);
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(char const *str, char c)
 {
 	char	**new_tab;
+	size_t	len;
 	int		i;
-	int		k;
-	int		j;
 
-	i = 0;
-	j = 0;
-	k = 0;
-	new_tab = malloc(sizeof(new_tab) * (ft_count_words(s, c) + 1));
-	if (!new_tab)
+	new_tab = (char **)malloc((ft_count_words(str, c) + 1) * sizeof(char *));
+	if (!new_tab || !str)
 		return (NULL);
-	while (i < ft_count_words(s, c))
-	{
-		new_tab[i++] = malloc(sizeof(char *) * (ft_strlen_sep(s, c)) + 1);
-		if (!new_tab[i])
-			return (NULL);
-	}
-	while (j < ft_count_words(s, c))
-	{
-		while (s[i])
-		{
-			if (s[i] == c && i == 0)
-				i = 0;
-			else if (s[i] != c)
-				new_tab[j][k++] = s[i];
-			else if ((s[i] == c || s[i] == '\0') && i != 0)
-				new_tab[j][k] = '\0';
-			i++;
-		}
-		k = 0;
-		j++;
-	}
-	return (new_tab);
-}
-
-static int	ft_strlen_sep(char const *s, char c)
-{
-	int	i;
-	int	cmp;
-
 	i = 0;
-	cmp = 0;
-	while (s[i] != '\0')
+	while (*str)
 	{
-		if (s[i] == c && i == 0)
-			cmp = 0;
-		else if (s[i] != c)
-			cmp++;
-		else if (s[i] == c)
-			return (cmp);
-		i++;
+		while (*str == c && *str)
+			str++;
+		if (*str)
+		{
+			if (!ft_strchr(str, c))
+				len = ft_strlen(str);
+			else
+				len = ft_strchr(str, c) - str;
+			new_tab[i++] = ft_substr(str, 0, len);
+			str += len;
+		}
 	}
-	return (0);
+	new_tab[i] = NULL;
+	return (new_tab);
 }
 
 static int	ft_count_words(char const *str, char c)
 {
 	int	count_wd;
-	int	i;
-
-	i = 0;
+	
+	if (!*str)
+		return (0);
 	count_wd = 0;
-	while (str[i])
+	while (*str)
 	{
-		if ((str[i] != c && str[i + 1] == c) || str[i + 1] == '\0')
+		while(*str == c)
+			str++;
+		if (*str)
 			count_wd++;
-		i++;
+		while (*str != c && *str)
+			str++;
 	}
 	return (count_wd);
 }
